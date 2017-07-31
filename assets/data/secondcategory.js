092c2aea7dc3c10a5646f69bae20f80cb29c8ad3
hncgc1990 <hncgc1990@163.com>
2017-07-31 12:13:16 +0800

    补充二次类目的显示

---
+++ assets/data/secondcategory.js
@@ -0,0 +1,320 @@
+const secondcategory={
+  "code": 0,
+  "msg": "成功",
+  "body": {
+    "brandList": [
+      {
+        "brandId": 1452,
+        "brandLogo": "http://haitao.nos.netease.com/icc1uivj43.jpg",
+        "brandName": "LANCÔME 兰蔻",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "兰蔻"
+      },
+      {
+        "brandId": 1566,
+        "brandLogo": "http://haitao.nos.netease.com/idvqc2kv42.jpg",
+        "brandName": "ESTĒE LAUDER 雅诗兰黛",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "雅诗兰黛"
+      },
+      {
+        "brandId": 1883,
+        "brandLogo": "http://haitao.nos.netease.com/idv0dp6a35.jpg",
+        "brandName": "CLINIQUE 倩碧",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "倩碧"
+      },
+      {
+        "brandId": 1447,
+        "brandLogo": "http://haitao.nosdn5.127.net/iexpqhz853_300_300.jpg",
+        "brandName": "Kiehl's 科颜氏",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "科颜氏"
+      },
+      {
+        "brandId": 2100,
+        "brandLogo": "http://haitao.nos.netease.com/1bk8ndmup45_300_300.jpg",
+        "brandName": "SK-II",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "SK-II"
+      },
+      {
+        "brandId": 2097,
+        "brandLogo": "http://haitao.nosdn1.127.net/ieft6s0945_120_120.jpg",
+        "brandName": "LA MER 海蓝之谜",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "海蓝之谜"
+      },
+      {
+        "brandId": 2195,
+        "brandLogo": "http://haitao.nosdn5.127.net/igatsf5q34_300_300.jpg",
+        "brandName": "POLA 宝丽",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "POLA"
+      },
+      {
+        "brandId": 1838,
+        "brandLogo": "http://haitao.nosdn1.127.net/ifxc1wa478_300_300.jpg",
+        "brandName": "Whoo 后",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "后"
+      }
+    ],
+    "level2CategoryList": [
+      {
+        "categoryId": 8173,
+        "categoryName": "彩妆香氛",
+        "childCategoryViewList": [
+          {
+            "categoryId": 8268,
+            "categoryName": "唇妆",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdp3l216_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8270,
+            "categoryName": "眼妆",
+            "iconUrl": "http://haitao.nosdn5.127.net/ipcdp8257_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8281,
+            "categoryName": "精油",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdpcsm2_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8283,
+            "categoryName": "美甲",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdpigg38_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8301,
+            "categoryName": "美容工具",
+            "iconUrl": "http://haitao.nos.netease.com/ipcdpnh734_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8302,
+            "categoryName": "香水",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdpsmx32_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8303,
+            "categoryName": "面部彩妆",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdq39p31_180_180.jpg",
+            "parentId": 8173
+          },
+          {
+            "categoryId": 8304,
+            "categoryName": "底妆",
+            "iconUrl": "http://haitao.nosdn2.127.net/ipcdq8ch83_180_180.jpg",
+            "parentId": 8173
+          }
+        ],
+        "hotSaleLinkUrl": "http://m.kaola.com/ranking/hotSale/8173.html",
+        "parentId": 8171
+      },
+      {
+        "categoryId": 8172,
+        "categoryName": "美容护肤",
+        "childCategoryViewList": [
+          {
+            "categoryId": 8174,
+            "categoryName": "面部清洁",
+            "iconUrl": "http://haitao.nos.netease.com/ipbyqbwy44_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8203,
+            "categoryName": "化妆水",
+            "iconUrl": "http://haitao.nos.netease.com/ipbyql7q47_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8205,
+            "categoryName": "面膜",
+            "iconUrl": "http://haitao.nos.netease.com/ipbyqpqd46_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8206,
+            "categoryName": "面霜",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipbyqut060_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8208,
+            "categoryName": "乳液",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipbyr0007_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8209,
+            "categoryName": "喷雾",
+            "iconUrl": "http://haitao.nos.netease.com/ipbyr50q5_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8210,
+            "categoryName": "面部精华",
+            "iconUrl": "http://haitao.nosdn5.127.net/ipbyrauc32_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8211,
+            "categoryName": "唇部护理",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipbyrg5145_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8212,
+            "categoryName": "防晒修复",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipcdnbld0_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8213,
+            "categoryName": "眼部护理",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdnh6w78_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8215,
+            "categoryName": "T区护理",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipcdnrmd28_180_180.jpg",
+            "parentId": 8172
+          },
+          {
+            "categoryId": 8216,
+            "categoryName": "护肤套装",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdnyvg7_180_180.jpg",
+            "parentId": 8172
+          }
+        ],
+        "hotSaleLinkUrl": "http://m.kaola.com/ranking/hotSale/8172.html",
+        "parentId": 8171
+      },
+      {
+        "categoryId": 8192,
+        "categoryName": "身体护理",
+        "childCategoryViewList": [
+          {
+            "categoryId": 8316,
+            "categoryName": "手部护理",
+            "iconUrl": "http://haitao.nos.netease.com/ipcdqosy8_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8318,
+            "categoryName": "胸部护理",
+            "iconUrl": "http://haitao.nosdn5.127.net/ipcdqt8n90_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8319,
+            "categoryName": "足部护理",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdqxvi52_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8321,
+            "categoryName": "身体乳/霜",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdra7574_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8322,
+            "categoryName": "脱毛膏",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdrgz481_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8324,
+            "categoryName": "止汗/去异味",
+            "iconUrl": "http://haitao.nosdn4.127.net/ipcdrn4566_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8325,
+            "categoryName": "磨砂/去角质",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipcdrtuu47_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 8326,
+            "categoryName": "身体护理套装",
+            "iconUrl": "http://haitao.nosdn1.127.net/ipcdrytu79_180_180.jpg",
+            "parentId": 8192
+          },
+          {
+            "categoryId": 10832,
+            "categoryName": "颈部护理",
+            "iconUrl": "http://haitao.nosdn3.127.net/isfgqfxf20_180_180.jpg",
+            "parentId": 8192
+          }
+        ],
+        "hotSaleLinkUrl": "http://m.kaola.com/ranking/hotSale/8192.html",
+        "parentId": 8171
+      },
+      {
+        "categoryId": 8193,
+        "categoryName": "男士护肤",
+        "childCategoryViewList": [
+          {
+            "categoryId": 8329,
+            "categoryName": "男士洁面",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipcdsdqd6_180_180.jpg",
+            "parentId": 8193
+          },
+          {
+            "categoryId": 10842,
+            "categoryName": "男士爽肤水",
+            "iconUrl": "http://haitao.nosdn5.127.net/isfihew975_180_180.jpg",
+            "parentId": 8193
+          },
+          {
+            "categoryId": 10837,
+            "categoryName": "男士面膜",
+            "iconUrl": "http://haitao.nos.netease.com/isfhfko540_180_180.jpg",
+            "parentId": 8193
+          },
+          {
+            "categoryId": 8333,
+            "categoryName": "男士面部乳霜",
+            "iconUrl": "http://haitao.nosdn3.127.net/ipcdslky85_180_180.jpg",
+            "parentId": 8193
+          },
+          {
+            "categoryId": 10844,
+            "categoryName": "男士面部精华",
+            "iconUrl": "http://haitao.nosdn4.127.net/isfili4s11_180_180.jpg",
+            "parentId": 8193
+          },
+          {
+            "categoryId": 10830,
+            "categoryName": "男士防晒",
+            "iconUrl": "http://haitao.nos.netease.com/isffjb6r2_180_180.jpg",
+            "parentId": 8193
+          }
+        ],
+        "hotSaleLinkUrl": "http://m.kaola.com/ranking/hotSale/8193.html",
+        "parentId": 8171
+      }
+    ]
+  }
+}
+
+export {
+    secondcategory
+}
--- pages/category/index.js
+++ pages/category/index.js
@@ -2,6 +2,7 @@
 
 import { category} from "../../assets/data/category"
 import { childCategory } from "../../assets/data/categorychild"
+import {secondcategory} from "../../assets/data/secondcategory"
 Page({
 
   /**
@@ -12,22 +13,41 @@ Page({
     hotKey:'春雨面膜',
     selected:0,
     brandList:[],
-    hotCategoryList:[]
+    hotCategoryList:[],
+    albumList:[],
+    data:{}
   },
 
   changeSelect:function(e){//左侧类目点击
     console.log(e.currentTarget.dataset.index)
     this.setData({
       selected: e.currentTarget.dataset.index,
+      data:e.currentTarget.dataset.index==0?childCategory:secondcategory
     })
 
 
   },
   onLoad: function (options){
+      let more={
+        "brandId": -1,
+        "brandLogo": "/assets/images/goto_all_brands.png",
+        "brandName": "更多",
+        "favorCount": 0,
+        "isFocus": 0,
+        "keyWords": "更多"
+      }
+
+      childCategory.body.brandList.push(more)
+      secondcategory.body.brandList.push(more)
+
+
+
       this.setData({
         categories: category.body.categoryTreeMenuList,
         brandList:childCategory.body.brandList,
-hotCategoryList: childCategory.body.hotCategoryList
+        hotCategoryList: childCategory.body.hotCategoryList,
+        albumList:childCategory.body.albumList,
+        data:childCategory
       })
   }
 })
